334bce81e65bf0cbb62538586e28f0a9c8da5c19
Paul Wildenhain <wildenhaip@chop.edu>
2021-04-05 17:12:36 -0400

    :file_folder: Restructure app routes into modules

---
+++ api/forms.py
@@ -0,0 +1,97 @@
+from typing import List, Optional
+import datetime
+
+from fastapi import APIRouter, Form, Depends
+from starlette.responses import RedirectResponse
+from starlette.status import HTTP_303_SEE_OTHER
+from sqlalchemy.orm import Session
+
+from db import schemas
+from api.dependencies import get_db
+from api.api import add_transaction, record_payday
+
+# no prefix here, since we want to redirect the user back to the home page
+router = APIRouter()
+
+
+@router.post(
+    "/form/add-transaction", response_class=RedirectResponse, include_in_schema=False
+)
+def add_transaction_form(
+    account_id: int = Form(...),
+    amount: int = Form(...),
+    comment: Optional[str] = Form(""),
+    transaction_type: Optional[str] = Form("debit"),
+    db: Session = Depends(get_db),
+):
+
+    transaction = schemas.TransactionCreate(
+        account_id=account_id,
+        amount=amount,
+        comment=comment,
+        transaction_type=transaction_type,
+        date=datetime.datetime.now(),
+    )
+
+    add_transaction(transaction, db)
+
+    return RedirectResponse(url="/ui/add-transaction", status_code=HTTP_303_SEE_OTHER)
+
+
+@router.post(
+    "/form/add-income", response_class=RedirectResponse, include_in_schema=False
+)
+def add_income_form(
+    account_id: str = Form(...),
+    amount: int = Form(...),
+    comment: Optional[str] = Form(""),
+    transaction_type: Optional[str] = Form("credit"),
+    db: Session = Depends(get_db),
+):
+    transaction = schemas.TransactionCreate(
+        account_id=account_id,
+        amount=amount,
+        comment=comment,
+        transaction_type=transaction_type,
+        date=datetime.datetime.now(),
+    )
+
+    add_transaction(transaction, db)
+
+    return RedirectResponse(url="/ui", status_code=HTTP_303_SEE_OTHER)
+
+
+@router.post("/form/transfer", response_class=RedirectResponse, include_in_schema=False)
+def transfer_form(
+    from_account_id: int = Form(...),
+    to_account_id: int = Form(...),
+    amount: int = Form(...),
+    db: Session = Depends(get_db),
+):
+    account_withdraw_transaction = schemas.TransactionCreate(
+        account_id=from_account_id,
+        amount=amount,
+        comment=f"Transfer to {from_account_id}",
+        transaction_type="debit",
+        date=datetime.datetime.now(),
+    )
+
+    add_transaction(account_withdraw_transaction, db)
+
+    account_deposit_transaction = schemas.TransactionCreate(
+        account_id=to_account_id,
+        amount=amount,
+        comment=f"Transfer from {to_account_id}",
+        transaction_type="credit",
+        date=datetime.datetime.now(),
+    )
+
+    add_transaction(account_deposit_transaction, db)
+
+    return RedirectResponse(url="/ui", status_code=HTTP_303_SEE_OTHER)
+
+
+@router.post("/form/payday", response_class=RedirectResponse, include_in_schema=False)
+def payday_form(payday: List[schemas.Transaction] = Depends(record_payday)):
+    # By adding the record_payday dependency, we're implicitly recording a payday
+    return RedirectResponse(url="/ui", status_code=HTTP_303_SEE_OTHER)
---
+++ api/views.py
@@ -0,0 +1,51 @@
+from typing import List
+
+from fastapi import APIRouter, Depends, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+from db import schemas
+from api.api import get_accounts
+
+templates = Jinja2Templates(directory="templates")
+
+router = APIRouter(prefix="/ui")
+
+
+@router.get("/", response_class=HTMLResponse, include_in_schema=False)
+def root(request: Request, accounts: List[schemas.Account] = Depends(get_accounts)):
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "accounts": accounts}
+    )
+
+
+@router.get("/add-transaction", response_class=HTMLResponse, include_in_schema=False)
+def add_transaction_view(
+    request: Request, accounts: List[schemas.Account] = Depends(get_accounts)
+):
+    return templates.TemplateResponse(
+        "add_transaction.html", {"request": request, "accounts": accounts}
+    )
+
+
+@router.get("/add-income", response_class=HTMLResponse, include_in_schema=False)
+def add_income_view(
+    request: Request, accounts: List[schemas.Account] = Depends(get_accounts)
+):
+    return templates.TemplateResponse(
+        "add_income.html", {"request": request, "accounts": accounts}
+    )
+
+
+@router.get("/transfer", response_class=HTMLResponse, include_in_schema=False)
+def transfer_view(
+    request: Request, accounts: List[schemas.Account] = Depends(get_accounts)
+):
+    return templates.TemplateResponse(
+        "transfer.html", {"request": request, "accounts": accounts}
+    )
+
+
+@router.get("/record-payday", response_class=HTMLResponse, include_in_schema=False)
+def record_payday_view(request: Request):
+    return templates.TemplateResponse("record_payday.html", {"request": request})
