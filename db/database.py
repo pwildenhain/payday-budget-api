7aaea2e61575a014c6dedd32f2f291a824724761
Paul Wildenhain <wildenhaip@chop.edu>
2021-05-12 11:09:35 -0400

    :alembic: Add test database

--- db/database.py
+++ db/database.py
@@ -1,8 +1,10 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./db/payday_budget.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DB_URL", "sqlite:///./db/test.db")
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
